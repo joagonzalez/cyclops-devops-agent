5a7578fa3094cd21e0e836585104687f3f85cec2
Joaquin Gonzalez <joagonzalez@gmail.com>
2025-05-25 19:38:03 -0300

    #10 adding prometheus class and refactoring promQL endpoints

--- src/api/promql.py
+++ src/api/promql.py
@@ -2,32 +2,35 @@
 API PromQL endpoints used to get data from Prometheus
 from external tools and applications
 """
-import httpx
 from typing import Dict, Any
 from fastapi import APIRouter, status
 from src.config.settings import config
+from src.services.prometheus import PrometheusClient
 
 
 router = APIRouter()
-
+prometheusClient = PrometheusClient(config["PROMETHEUS_URL"])
 
 @router.get("/query/", tags=["PromQL"], status_code=status.HTTP_200_OK)
 async def get_promql_health() -> Dict[str, Any]:
-    """Checks Prometheus health by making a test query
+    """
+    Checks Prometheus health by making a test query
 
     Returns:
         Dict[str, str]: dict with status message
     """
-    prometheus_url = config["PROMETHEUS_URL"]
-    query_url = f"{prometheus_url}/api/v1/query"
-    params = {"query": "up"}
+    return prometheusClient.test_connection()
 
-    try:
-        async with httpx.AsyncClient() as client:
-            response = await client.get(query_url, params=params, timeout=5)
-            if response.status_code == 200:
-                return {"status": "OK", "data": response.json()}
-            else:
-                return {"status": f"Prometheus error: {response.status_code}"}
-    except Exception as e:
-        return {"status": f"Error: {str(e)}"}
+
+@router.get("/query/{promql}", tags=["PromQL"], status_code=status.HTTP_200_OK)
+async def get_promql_query(promql: str) -> Dict[str, Any]:
+    """
+    Executes a generic PromQL query
+
+    Args:
+        promql (str): The PromQL query string.
+
+    Returns:
+        Dict[str, Any]: The response from Prometheus.
+    """
+    return prometheusClient.query(promql)
